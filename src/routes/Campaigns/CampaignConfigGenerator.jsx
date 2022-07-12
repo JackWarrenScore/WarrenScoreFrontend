e18079304fb55d798889560868768a7e633fef64
JackWarrenScore <106928849+JackWarrenScore@users.noreply.github.com>
2022-07-11 21:35:15 -0700

    Finished rough designs of config page. Need to get posting working correctly.

--- src/routes/Campaigns/CampaignConfigGenerator.jsx
+++ src/routes/Campaigns/CampaignConfigGenerator.jsx
@@ -1,52 +1,77 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
-import Button from 'react-bootstrap/Button';
+import { Button } from 'react-bootstrap';
+import RangeSlider from 'react-bootstrap-range-slider';
 
 import TopMenu from "../../components/TopMenu"
+import PlusMinusButton from '../../components/PlusMinusButton';
 import ToggleButtons from '../../components/ToggleButtons';
+
 import React, { useState } from 'react';
 import { useParams, useNavigate } from "react-router-dom";
-import RangeSlider from 'react-bootstrap-range-slider';
-import PlusMinusButton from '../../components/PlusMinusButton';
+
+import axios from 'axios';
 
 export default function CampaignConfigGenerator(){
 
-    function generateConfigPreset(shapeMaxSize, plusModifier, timesModifier, powerModifier, undefinedModifier){
+    const { campaignId } = useParams();
+    let navigate = useNavigate();
+
+    function generateConfigPreset(shapeMaxSize, plusModifier, timesModifier, powerModifier, undefinedModifier, radiusMax, scoreType){
         return {
             SHAPE_MAX: shapeMaxSize,
             "+": plusModifier,
             "*": timesModifier,
             "^": powerModifier,
-            "u": undefinedModifier
+            "u": undefinedModifier,
+            "RADIUS_MAX": radiusMax,
+            "SCORE_TYPE": scoreType
         }
+    }
+
+    //TODO: Send data to backend
+    function saveAndContinue() {
+        axios.post('http://localhost:3000/upsertCampaignConfig', {
+            campaignInfo
+        })
+        .then(function (response){
+            console.log(response);
+            navigate(`../campaigns/${campaignId}/campaign-mailing-list`)
+        })
+    }
+
+    function updateKeyByValue(keyForNewValue, newValue){
+        let campaignInfoCopy = JSON.parse(JSON.stringify(campaignInfo));
+        campaignInfoCopy[keyForNewValue] = newValue;
+        console.log(campaignInfoCopy);
+        setCampaignInfo(campaignInfoCopy);
     }
 
     const rolePresetData = [
         {
             'name': 'Custom', 'value':'1', 'variant': "secondary", 'variantOutline': "outline-secondary", 
-            'dataToBeModified': generateConfigPreset(1, 0, 0, 0, 0)    
+            'dataToBeModified': generateConfigPreset(1, 0, 0, 0, 0, 5, "MAX")
         },
         {
             'name': 'Manager', 'value':'2', 'variant': "success", 'variantOutline': "outline-success",
-            'dataToBeModified': generateConfigPreset(1, 1, 1, 0, 2)
+            'dataToBeModified': generateConfigPreset(1, 1, 1, 0, 2, 3, "MAX")
         },
         {
             'name': 'Analyst', 'value':'3', 'variant': "warning", 'variantOutline': "outline-warning",
-            'dataToBeModified': generateConfigPreset(1, 1, 2, 0, 0)
+            'dataToBeModified': generateConfigPreset(1, 1, 2, 0, 0, 5, "MAX")
         },
         {
             'name': 'Developer', 'value':'4', 'variant': "danger", 'variantOutline': "outline-danger",
-            'dataToBeModified': generateConfigPreset(3, 0, 1, 2, 1)
+            'dataToBeModified': generateConfigPreset(3, 0, 1, 2, 1, 5, "MAX")
         },
         {
             'name': 'Architect', 'value':'5', 'variant': "info", 'variantOutline': "outline-info",
-            'dataToBeModified': generateConfigPreset(5, 1, 2, 3, 0)
+            'dataToBeModified': generateConfigPreset(5, 1, 2, 3, 0, 10, "MIN")
         }
     ]
 
-    const { campaignId } = useParams();
-    let navigate = useNavigate();
+
 
     const [campaignInfo, setCampaignInfo] = useState({
         TITLE:"A pretty sick title.",
@@ -55,20 +80,10 @@ export default function CampaignConfigGenerator(){
         "+":0,
         "*":0,
         "^":0,
-        "u":0
+        "u":0,
+        "RADIUS_MAX":1,
+        "SCORE_TYPE":"MAX"
     });
-
-    //TODO: Send data to backend
-    function saveAndContinue() {
-        console.log(campaignInfo)
-        navigate(`../campaigns/${campaignId}/campaign-mailing-list`)
-    }
-
-    function updateKeyByValue(keyForNewValue, newValue){
-        let campaignInfoCopy = JSON.parse(JSON.stringify(campaignInfo));
-        campaignInfoCopy[keyForNewValue] = newValue;
-        setCampaignInfo(campaignInfoCopy);
-    }
 
     return (
         <div>
@@ -94,14 +109,32 @@ export default function CampaignConfigGenerator(){
                     />
                 </div>
 
-                <div>
+                <div style={{"margin": "auto", "width": "75" + "%"}}>
                     <PlusMinusButton state={campaignInfo} update={updateKeyByValue} keyName={"+"}/>
                     <PlusMinusButton state={campaignInfo} update={updateKeyByValue} keyName={"*"}/>
                     <PlusMinusButton state={campaignInfo} update={updateKeyByValue} keyName={"^"}/>
                     <PlusMinusButton state={campaignInfo} update={updateKeyByValue} keyName={"u"}/>
                 </div>
 
+                <div style={{width: 25 + '%'}}>
+                    Radius Range Max:
+                        <RangeSlider
+                            min={1}
+                            max={10}
+                            value={campaignInfo.RADIUS_MAX}
+                            onChange={e => updateKeyByValue("RADIUS_MAX", e.target.value)}
+                            tooltipLabel={currentValue => `${currentValue}`}
+                            tooltip='on'
+                        />
+                </div>
 
+                <div>
+                    <Button variant="warning" onClick={() => updateKeyByValue("SCORE_TYPE", "MAX")}>Max Score</Button>
+                    <Button variant="success" onClick={() => updateKeyByValue("SCORE_TYPE", "MIN")}>Min Score</Button>
+                    <p>Current Win Condition: {campaignInfo.SCORE_TYPE}</p>
+                </div>
+
+                <br/>
                 <Button variant="primary" onClick={() => saveAndContinue()}>Save And Continue</Button>
         </div>
     )
