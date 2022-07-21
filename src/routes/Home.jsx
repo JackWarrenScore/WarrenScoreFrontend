4ba7acf888a744aaad827a0ccbdf031abc423c42
JackWarrenScore <106928849+JackWarrenScore@users.noreply.github.com>
2022-07-20 19:35:48 -0700

    Made a self deprecating joke.
    Thought I'd be making more today.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,6 +7,10 @@ export default function Home(){
             <div>
                 <h6>Warren score is...</h6>
                 <h2>A diagnostic tool for technical recruiting</h2>
+                <br/>
+                <h3>Incredibly important sounding poppycock here.</h3>
+                <h3>Oh Warren Score is so important.</h3>
+                <h3>**More self aggrandizing statements**</h3>
             </div>
         </div>
     )
